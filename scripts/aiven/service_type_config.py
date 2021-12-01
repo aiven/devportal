24b2f9f7999f5d0328e05f181ca3384a40609d62
Lorna Mitchell <lornajane@aiven.io>
2021-12-01 18:36:24 +0000

    Move from table to sort of list layout, include nested values

--- scripts/aiven/service_type_config.py
+++ scripts/aiven/service_type_config.py
@@ -2,29 +2,43 @@ import requests
 import argparse
 
 
-def print_row(param, value_type, desc):
-    print(f"  * - {param}")
-    print(f"    - {value_type}")
-    print(f"    - {desc}")
+def print_row(param, value_type, title, desc, indent=0):
+    preamble = ''
+    for i in range(indent):
+        preamble = preamble + '  '
+
+    print(f"{preamble}{param}  *{value_type}*")
+    print(f"{preamble}  - **{title}** {desc}")
+    print("")
 
 
 def print_service_type_docs(service_type, data):
     schema = data['service_types'][service_type]['user_config_schema']
 
     print("")
-    # Table header
-    print(".. list-table::")
-    print("  :header-rows: 1")
-    print("")
-    print_row("Parameter", "Value Type", "Description")
 
     # Rows
     for key, value in schema['properties'].items():
         print_row(
             f"``{key}``",
             value.get('type', ''),
-            value.get('title') + " " + value.get("description", '')
+            value.get('title'),
+            value.get("description", '')
             )
+
+        # handle any nested properties
+        if value.get('type', '') == "object":
+            for nested_key, nested_value in value.get('properties').items():
+                print_row(
+                    f"``{nested_key}``",
+                    nested_value.get('type', ''),
+                    nested_value.get('title'),
+                    nested_value.get('description'),
+                    1
+                    )
+
+        print("")
+        print("")
         pass
     # Empty row to end
     print("")
