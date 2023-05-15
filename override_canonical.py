f0e5c2eafcdc9f168b431f09954c3eef5011f086
angelinekwan <angeline.kwan@aiven.io>
2023-05-15 18:22:16 +0300

    add comments

--- override_canonical.py
+++ override_canonical.py
@@ -1,3 +1,8 @@
+# The canonical tags of docs.aiven.io are pointing to redirected URL versions 
+# ending with .html causing a redirect-canonical loop and makes the pages non-indexable.
+# This will overwrite the canonical tag defined in furo base.html template
+# Note that this method might not work if the Furo theme or Sphinx changes how they handle 
+# the canonical URL in future releases.
 def override_canonical_url(app, pagename, templatename, context, doctree):
     context['pageurl'] = "https://docs.aiven.io/{}".format(pagename)
 
