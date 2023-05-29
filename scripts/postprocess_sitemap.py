39f846e546ec45fbbd17e29530f21bd5858b83fd
angelinekwan <angeline.kwan@aiven.io>
2023-05-29 21:40:38 +0300

    using beautifulsoup and lxml to preprocess generated sitemap exclude html extension from url

---
+++ scripts/postprocess_sitemap.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+
+with open('./_build/sitemap.xml', 'r') as f:
+    contents = f.read()
+
+soup = BeautifulSoup(contents, 'lxml')
+
+urls = soup.find_all('loc')
+
+for url in urls:
+    text = url.string
+    # Skip the 404 page and the genindex page
+    if '404' in text or 'genindex' in text:
+        continue
+    if text.endswith('index.html'):
+        url.string = text[:-10]  # removes the "index.html"
+    elif text.endswith('.html'):
+        url.string = text[:-5]  # removes the ".html"
+
+# Remove all URLs that contain '404' or 'genindex'
+urls = [url for url in urls if '404' not in url.string and 'genindex' not in url.string]
+
+with open('./_build/sitemap.xml', 'w') as f:
+    for url in urls:
+        f.write(str(url))
